e5467b73f6cdad1d7e14a135cb1698ba3753ae72
tonis <tonisives@gmail.com>
2021-02-24 14:04:39 +0700

    add kermit logs

--- shared/src/commonMain/kotlin/com/tonisives/githubbrowser/shared/Greeting.kt
+++ shared/src/commonMain/kotlin/com/tonisives/githubbrowser/shared/Greeting.kt
@@ -1,8 +1,15 @@
 package com.tonisives.githubbrowser.shared
 
+import co.touchlab.kermit.CommonLogger
+import co.touchlab.kermit.Kermit
+
+val kermit = Kermit(CommonLogger())
 
 class Greeting {
+
     fun greeting(): String {
-        return "Hello, ${Platform().platform}!"
+        val message = "Hello, ${Platform().platform}!"
+        kermit.i { message }
+        return message
     }
 }
